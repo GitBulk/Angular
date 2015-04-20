25a6684121f059187887fd162fd3d51f5b0e313c
GitBulk <giacatiensinh@hotmail.com>
2015-04-20 13:14:34 +0700

    update
    function

--- Fund/app/js/services/GravatarUrlBuilder.js
+++ Fund/app/js/services/GravatarUrlBuilder.js
@@ -1,18 +1,18 @@
 'use strict';
+<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js"></script>
 
 app.factory('gravatarUrlBuilder', ['', function(){
 	return function createUrl(email){
 		var defaultUrl = "http://www.gravatar.com/avatar/000?s=200";
-		var regex = /^#/; // email regex
+		var regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;; // email regex
 
 		if (!regex.test(email)) {
 			return defaultUrl;
 		}
 
-		var md5 = function(input) {
-			return "";
-		};
+		var hash = CryptoJS.MD5(email);
+		console.log(hash);
 
-		return "http://www.gravatar.com/avatar/" + md5(email) + ".jpg?s=200&r=g";
+		return "http://www.gravatar.com/avatar/" + hash + ".jpg?s=200&r=g";
 	};
 }])
