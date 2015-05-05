b0e495cf251fa6ca6be1351c8dbd38986c4fea2f
GitBulk <giacatiensinh@hotmail.com>
2015-05-05 13:25:05 +0700

    update interface of driver details
    update interface of driver details

--- Racer/app/scripts/services/CarFactory.js
+++ Racer/app/scripts/services/CarFactory.js
@@ -1,5 +1,7 @@
-app.service('CarService', ['$http', function($http){
-	this.getDrivers = function() {
+app.factory('CarFactory', ['$http', function($http){
+	var obj = {};
+
+	obj.getDrivers = function() {
 		// to do
 		return $http({
 			method: "JSONP",
@@ -7,17 +9,19 @@ app.service('CarService', ['$http', function($http){
 		});
 	}
 
-	this.getDriverDetails = function(id) {
+	obj.getDriverDetails = function(id) {
 		return $http({
 			method: "JSONP",
 			url: "http://ergast.com/api/f1/2015/drivers/"+ id + "/driverStandings.json?callback=JSON_CALLBACK"
 		});
 	}
 
-	this.getDriverRaces = function(id) {
+	obj.getDriverRaces = function(id) {
 		return $http({
 			method: "JSONP",
 			url: "http://ergast.com/api/f1/2015/drivers/"+ id + "/results.json?callback=JSON_CALLBACK"
 		});
 	}
+
+	return obj;
 }]);
